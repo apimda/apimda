a86987e0cf0ff0867a58de7f43e9a31d8058c0d2
Joseph Mays <jmays5150@gmail.com>
2023-01-07 12:32:07 +0100

    prepare docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'joemays', // Usually your GitHub org/user name.
+  organizationName: 'apimda', // Usually your GitHub org/user name.
   projectName: 'apimda', // Usually your repo name.
   trailingSlash: false,
 
